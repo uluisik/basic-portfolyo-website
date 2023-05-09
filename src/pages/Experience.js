763f9be3fec009aceb68d9b0a4ae82fce46d67b9
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 20:50:09 +0300

    added helper

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,35 @@
+import { BusinessCenter, School } from "@mui/icons-material";
 import React from "react";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="expContainer">
+      <div className="expElements">
+        <ol>
+          <li>
+            <School />
+            <h3>High School Graduation 2011</h3>
+            <p>Akpinar Anatolian Teacher's High School</p>
+          </li>
+          <li>
+            <School />
+            <h3>University Graduation 2015</h3>
+            <p>Military Academy Industrial Engineering</p>
+          </li>
+          <li>
+            <School />
+            <h3>University Graduation 2015</h3>
+            <p>Military Academy Industrial Engineering</p>
+          </li>
+          <li>
+            <BusinessCenter />
+            <h3>My First Job As a Front-End Dev</h3>
+            <p>---</p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { ProjectList } from "../helper/ProjectList";
+import ProjectItem from "../components/ProjectItem";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
