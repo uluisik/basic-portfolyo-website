a4ced84c3d783b97aa9f5095971a8ebbccdc9709
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 16:27:40 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "@mui/icons-material";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
